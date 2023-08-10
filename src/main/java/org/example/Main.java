7294d96bb40a887cec12df3914c14f8e48f98537
Denis Pasha <denispasha95@gmail.com>
2023-08-10 19:50:24 +0300

    more exercises

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,43 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = {3,2,4,1};
+        int result = semiOrderedPermutation(arr);
+        System.out.println(result);
+
     }
+
+    public static int semiOrderedPermutation(int[] nums) {
+
+        int indexZero = nums[0];
+        int indexLast = nums[nums.length - 1];
+        int counter = 0;
+        int i = 0;
+        int j = 1;
+
+        while (nums[0] != 1 || nums[nums.length - 1] != nums.length){
+
+            if (j >= nums.length){
+                i = 0;
+                j = 1;
+            }
+
+            if (nums[i] > nums[j]){
+                counter ++;
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }else {
+                i++;
+                j++;
+            }
+
+        }
+
+        return counter;
+
+    }
+
 }
--- src/main/java/org/example/leetCode75Exercises/Main.java
+++ src/main/java/org/example/leetCode75Exercises/Main.java
@@ -1,12 +1,59 @@
 package org.example.leetCode75Exercises;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
+        int[] arr = {0,0,9,0,0};
+
+        int[] answer = productExceptSelf(arr);
+        System.out.println();
 
     }
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int length = nums.length;
+        int[] toReturn = new int[length];
+
+        int i = 0;
+
+        while (i < nums.length){
+            int[] left = new int[i + 1];
+            int[] right = new int[nums.length - (i + 1)];
+
+
+
+            int leftSum = getLeftSum(nums , i);
+            int rightSum = getRightSum(nums , i);
+
+            toReturn[i] = leftSum * rightSum;
+
+            i++;
+        }
+
+
+
+        return toReturn;
+    }
+
+    private static int getLeftSum(int[] nums, int i) {
+        int toReturn = 1;
+        for (int j = 0; j < i; j++) {
+            toReturn *= nums[j];
+        }
+        return toReturn;
+    }
+
+    private static int getRightSum(int[] nums, int i) {
+        int toReturn = 1;
+        for (int j = i + 1; j < nums.length; j++) {
+            toReturn *= nums[j];
+        }
+        return toReturn;
+
+    }
+
+
 }
 
 
